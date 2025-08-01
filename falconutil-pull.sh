f810778a547742fbc4e3dfe3a5274b42e0d9dd49
Gus Vine <angus.vine@sweat.com>
2025-08-01 10:34:09 +0930

    remove quotes in command line

--- falconutil-pull.sh
+++ falconutil-pull.sh
@@ -37,7 +37,7 @@ VERSION=${INPUT_VERSION:+"--version ${INPUT_VERSION}"}
 # check if the falcon image platform is provided
 PLATFORM=${INPUT_FALCON_IMAGE_PLATFORM:+"--platform ${INPUT_FALCON_IMAGE_PLATFORM}"}
 
-output=$(bash falcon-container-sensor-pull.sh -u "${INPUT_FALCON_CLIENT_ID}" -r "${INPUT_FALCON_REGION}" -t falcon-container "${VERSION}" "${PLATFORM}")
+output=$(bash falcon-container-sensor-pull.sh -u "${INPUT_FALCON_CLIENT_ID}" -r "${INPUT_FALCON_REGION}" -t falcon-container ${VERSION} ${PLATFORM})
 
 # Extract the image name from the output
 image_name=$(echo "$output" | grep "^registry.*.com/falcon-container" | tail -n 1)
